6b80a71bd5ee6a06a37190d5d3b452f350934fab
G Jake Gebbie <ggebbie@whoi.edu>
2021-09-04 09:16:08 -0400

    clean up surfacepatch, cartesianindex

---
+++ src/make_project.jl
@@ -0,0 +1,35 @@
+""" 
+    A short code to show how this package was made
+    according to a template. This is a utility script that
+    was run just once to create the package and will not
+    be needed for this project again.
+
+    Steps: 
+    1. Use PkgTemplates to make git repo.
+    2. new empty repository on GitHub.
+    3. Then push an existing repository from the command line:
+    `git remote add origin git@github.com:ggebbie/TMI.jl.git`
+    `git branch -M main`
+    `git push -u origin main`
+
+    First two commands probably unnecessary.
+    I didn't figure out how to do this with magit in emacs.
+"""
+using Revise, PkgTemplates
+
+t = Template(; 
+    user="ggebbie",
+    dir="~/.julia/dev",
+    authors="G Jake Gebbie",
+    julia=v"1.5",
+    plugins=[
+        License(; name="MIT"),
+        Git(; manifest=true, ssh=true),
+        GitHubActions(; x86=true),
+        Codecov(),
+        Documenter{GitHubActions}(),
+        Develop(),
+    ],
+             )
+
+t("TMI.jl")
