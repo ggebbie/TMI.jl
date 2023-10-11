bb4ed2ab7cbb914349b6c591cbc528319031a815
cspencerjones <spencerjones@tamu.edu>
2023-10-11 14:21:04 -0500

    costfunction to fix output tracer field equal to input tracer field

--- scripts/invert_model_TS.jl
+++ scripts/invert_model_TS.jl
@@ -17,8 +17,8 @@ A, Alu, γ, TMIfile, L, B = config_from_nc(TMIversion)
 # first guess of change to surface boundary conditions
 # ocean values are 0
 #u = zerosurfaceboundary(γ)
-u = (;surface = zerosurfaceboundary(γ))
-uvec = vec(u)
+#u = (;surface = zerosurfaceboundary(γ))
+#uvec = vec(u)
   
 pkgdir() = dirname(dirname(pathof(TMI)))
 pkgdir(args...) = joinpath(pkgdir(), args...)
@@ -28,6 +28,16 @@ pkgdatadir(args...) = joinpath(pkgdatadir(), args...)
 
 TMIfile = pkgdatadir("TMI_"*TMIversion*".nc")
 θtrue = readfield(TMIfile,"θ",γ) 
+ctrue = vec(θtrue)
+
+# take firtst guess as ones
+θguess =  Field(ones(size(γ.wet)),γ,θtrue.name,θtrue.longname,θtrue.units)
+cvec=vec(θguess)
+
+#first guess control vector is zero
+u =  Field(zeros(size(γ.wet)),γ,θtrue.name,θtrue.longname,θtrue.units)
+uvec = vec(u)
+
 
 # a first guess: observed surface boundary conditions are perfect.
 # set surface boundary condition to the observations.
@@ -36,11 +46,11 @@ TMIfile = pkgdatadir("TMI_"*TMIversion*".nc")
 b = (;surface = getsurfaceboundary(θtrue))
 
 #We need an error covariance matrix
-W⁻ = (1/sum(γ.wet)) .* Diagonal(1 ./(10^(-2) .* ones(sum(γ.wet))).^2)
+W⁻ = Diagonal(1 ./( ones(sum(γ.wet))).^2)#(1/sum(γ.wet))
 
 # get sample J value
-F,G = costfunction_gridded_model(uvec,Alu,b,u,θtrue,W⁻,γ)
-fg!(F,G,x) = costfunction_gridded_model!(F,G,x,Alu,b,u,θtrue,W⁻,γ)
+F = costfunction_gridded_model(uvec,Alu,b,u,ctrue,cvec,W⁻,γ)
+fg!(F,G,x) = costfunction_gridded_model!(F,G,x,Alu,b,u,ctrue,cvec,W⁻,γ)
 
 #fg(x) = costfunction_gridded_model(x,Alu,b,u,θtrue,W⁻,γ)
 #f(x) = fg(x)[1]
@@ -64,21 +74,21 @@ println("Location for test =",ii)
 #### end gradient check #################
 
 # filter the data with an Optim.jl method
-iterations = 25
+iterations = 250
 out = steadyclimatology(uvec,fg!,iterations)
 
 # reconstruct by hand to double-check.
-ũ = unvec(u,out.minimizer)
+ũ = unvec((W⁻ * u),out.minimizer)
 
 # apply to the boundary conditions
-b̃ = adjustboundarycondition(b,ũ)
+#b̃ = adjustboundarycondition(b,ũ)
 
 # reconstruct tracer map
-c₀ = steadyinversion(Alu,b,γ)
-c̃  = steadyinversion(Alu,b̃,γ)
+c₀ = θguess
+c̃  = θguess+ũ
 
 Δc̃ = c̃ - θtrue
-Δc₀ = c₀ - θtrue
+Δc₀ = θguess - θtrue
 
 # plot the difference
 level = 15 # your choice 1-33
@@ -89,15 +99,16 @@ label = "Optimized misfit: Δc̃"
 planviewplot(Δc̃, depth, cntrs, titlelabel=label) 
 readline()
 
+cntrs = -15:0.5:15
 label = "First guess misfit: Δc₀"
 planviewplot(Δc₀, depth, cntrs, titlelabel=label) 
 readline()
 
-cntrs = -1:0.5:15
+cntrs = 0:0.5:15
 label = "True θ"
 planviewplot(θtrue, depth, cntrs, titlelabel=label)
 readline()
 
-cntrs = -1:0.5:15
+cntrs = 0:0.5:15
 label = "Optimized θ"
 planviewplot(c̃, depth, cntrs, titlelabel=label)
