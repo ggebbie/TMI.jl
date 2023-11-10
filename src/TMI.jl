6d02eb3ed1922d871bbd4b36c61bb5b35db8c63c
G Jake Gebbie <ggebbie@whoi.edu>
2023-11-10 16:03:11 -0500

    put grid into its own src file

--- src/TMI.jl
+++ src/TMI.jl
@@ -73,39 +73,7 @@ import Base: zeros, one, oneunit, ones,  (\)
 #import Base: maximum, minimum
 import Base: (+), (-), (*), (/), vec
 import LinearAlgebra: dot
-
-"""
-    struct Grid
-
-    TMI grid with accounting for wet/dry points
-"""
-struct Grid
-    lon::Vector{Float64}
-    lat::Vector{Float64}
-    depth::Vector{Float64}
-    wet::BitArray{3}
-    interior::BitArray{3}
-    #    I::Vector{CartesianIndex{3}} # index
-    #    R::Array{Int,3}
-    #    R::LinearIndices{3, Tuple{UnitRange{Int64}, UnitRange{Int64}, UnitRange{Int64}}} 
-end
-
-"""
-    Base.propertynames(γ::Grid) = (I,R,fieldnames(typeof(x))...)
-
-    Do not store Cartesian and linear indices.
-    Compute them on demand.
-""" 
-Base.propertynames(γ::Grid) = (:I,:R,fieldnames(typeof(γ))...)
-function Base.getproperty(γ::Grid, d::Symbol)
-    if d === :I
-        return cartesianindex(γ.wet)
-    elseif d === :R
-        return linearindex(γ.wet)
-    else
-        return getfield(γ, d)
-    end
-end
+include("grid.jl")
 
 """
     struct Field
