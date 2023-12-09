7ac6ce6d9dafdb4ef1d9e660fa17c2b169e006fb
G Jake Gebbie <ggebbie@whoi.edu>
2023-12-09 17:36:45 -0500

    ex0 bug fixes

--- scripts/ex0.tracerinversion.jl
+++ scripts/ex0.tracerinversion.jl
@@ -20,7 +20,7 @@ using GeoPythonPlot
 TMIversion = "modern_90x45x33_GH10_GH12"
 A, Alu, γ, TMIfile, L, B = config_from_nc(TMIversion);
 
-include("../utils/numerics_steadyinversion.jl")
+include(TMI.pkgutilsdir("phosphate_steadyinversion.jl"))
 
 ## Plot a plan view
 # view the surface
@@ -32,7 +32,7 @@ depth = γ.depth[level]
 label = PO₄total.longname*", depth = "*string(depth)*" m"
 
 # Help: needs work with continents and labels
-GGplot.pygui(true) # to help plots appear on screen using Python GUI
+GeoPythonPlot.pygui(true) # to help plots appear on screen using Python GUI
 planviewplot(PO₄total, depth, cntrs, titlelabel=label)
 # alternatively push to Julia backend (VS Code)
 # GGplot.display(GGplot.gcf())
@@ -43,10 +43,11 @@ lims = 0:0.1:3.0
 sectionplot(PO₄total,lon_section,lims)
 
 ## oxygen distribution
-yO₂ = readfield(TMIfile,"O₂",γ)
-bO₂ = getsurfaceboundary(yO₂)
-# set the optional stoichiometric ratio.
-O₂ = steadyinversion(Alu,bO₂,γ,q=qPO₄,r=-170.0)
+include(TMI.pkgutilsdir("oxygen_steadyinversion.jl"))
+# yO₂ = readfield(TMIfile,"O₂",γ)
+# bO₂ = getsurfaceboundary(yO₂)
+# # set the optional stoichiometric ratio.
+# O₂ = steadyinversion(Alu,bO₂,γ,q=qPO₄,r=-170.0)
 
 # Plan view of oxygen at same depth as phosphate
 # but different contours
