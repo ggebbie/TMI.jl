0b2d59331b4cd2b5088a47f31657ac36c7d66529
cspencerjones <spencerjones@tamu.edu>
2024-01-06 22:05:01 -0600

    can invert prelim. mitgcm with no errors in workflow

--- scripts/mit_inv.jl
+++ scripts/mit_inv.jl
@@ -11,11 +11,10 @@ using GeoPythonPlot
 using LinearAlgebra
 using SparseArrays
 using Statistics
+using JLD
 #, Distributions, LinearAlgebra,  Zygote, ForwardDiff, Optim
 
-TMIversion = "modern_90x45x33_GH10_GH12"
-A, Alu, γ, TMIfile, L, B = config_from_nc(TMIversion)
-
+TMIversion = "simple_mitgcm"
 
   
 pkgdir() = dirname(dirname(pathof(TMI)))
@@ -24,19 +23,23 @@ pkgdir(args...) = joinpath(pkgdir(), args...)
 pkgdatadir() = joinpath(pkgdir(),"data")
 pkgdatadir(args...) = joinpath(pkgdatadir(), args...)
 
-TMIfile = pkgdatadir("TMI_"*TMIversion*".nc")
-θtrue = readfield(TMIfile,"θ",γ)
+TMIfile = pkgdatadir(""*TMIversion*".nc")
+println(TMIfile)
+γ = Grid(TMIfile, "hFacC", "XC", "YC", "Z")
+θtrue = readfield(TMIfile,"THETA",γ)
 
-ctrue = vec(θtrue)
+ctrue = Float64.(vec(θtrue))
 
-println(typeof(ctrue))
+#println(typeof(ctrue))
+
 
 surfind = surfaceindex(γ.I)
 
 # The first guess for the tracer concentration should be close to the actual tracer concentration
 # take first guess as θtrue
-cvec=vec(θtrue)
+cvec=(vec(θtrue))
 θguess = unvec(θtrue,cvec)
+
 
 #first guess tracer control vector is near zero, and we want this to remain relatively small
 u =  Field(-0.01.*ones(size(γ.wet)),γ,θtrue.name,θtrue.longname,θtrue.units)
@@ -44,20 +47,19 @@ uvec = vec(u)
 q = zeros(size(uvec))
 q[surfind] = cvec[surfind]
 
-
 #We need an error covariance matrix
 W⁻ = Diagonal(1 ./( ones(sum(γ.wet))).^2)#(1/sum(γ.wet))
 
-#I want to allow a bunch of error in the surface part of the tracer conservation please
-Qerror = ones(size(γ.wet))
-Qerror[:,:,1].=0
-Qfield = Field(Qerror,γ,θtrue.name,θtrue.longname,θtrue.units)
-Qvec = vec(Qfield)
 
 Qdiag = 1 ./ ones(sum(γ.wet))
 #Qdiag[surfind] .= 10^(-7)
 Q⁻ = Diagonal(Qdiag)
-A0= A  .* 0.2
+
+ind_1 = load("ind_1.jld")["ind_1"]
+ind_2 = load("ind_2.jld")["ind_2"]
+
+A0 = sparse(ind_1, ind_2, ones(length(ind_1))*0.5)
+
 non_zero_indices1, non_zero_indices2, non_zero_values = findnz(A0)
 
 non_zero_indices = hcat(non_zero_indices1, non_zero_indices2)
@@ -75,32 +77,31 @@ J₀,gJ₀ = fg(convec)
 
 #### gradient check ###################
 # check with forward differences
-ϵ = 1e-3
-#ii = rand(1:sum(γ.wet[:,:,1]))
-println(size(length(convec)))
-ii = rand(1:length(convec))
-println("Location for test =",ii)
-δu = copy(convec); δu[ii] += ϵ
-∇f_finite = (f(δu) - f(convec))/ϵ
-println(∇f_finite)
+#ϵ = 1e-3
+#println(size(length(convec)))
+#ii = rand(1:length(convec))
+#println("Location for test =",ii)
+#δu = copy(convec); δu[ii] += ϵ
+#∇f_finite = (f(δu) - f(convec))/ϵ
+#println(∇f_finite)
 
-fg!(J₀,gJ₀,(convec+δu)./2) # J̃₀ is not overwritten
-∇f = gJ₀[ii]
-println(∇f)
+#fg!(J₀,gJ₀,(convec+δu)./2) # J̃₀ is not overwritten
+#∇f = gJ₀[ii]
+#println(∇f)
 
 # error less than 10 percent?
-println("Percent error ",100*abs(∇f - ∇f_finite)/abs(∇f + ∇f_finite))
+#println("Percent error ",100*abs(∇f - ∇f_finite)/abs(∇f + ∇f_finite))
 #### end gradient check #################
-
 
 #print(length(convec))
 # filter the data with an Optim.jl method
-iterations = 20
+iterations = 2
 out = steadyclimatology_optim(convec,fg!,iterations)
 
 # reconstruct by hand to double-check.
-ũ = unvec((W⁻ * u),out.minimizer[begin:ulength])
+ũ = unvec(θguess,Float32.(out.minimizer[begin:ulength]))
 
+#println(typeof(ũ))
 
 # reconstruct tracer map
 c₀ = θguess
@@ -110,40 +111,41 @@ c̃  = θguess+ũ
 Δc₀ = θguess - θtrue
 
 Anew = A0 + sparse(non_zero_indices[:, 1], non_zero_indices[:, 2], out.minimizer[ulength+1:end])
-onesvec = ones(size(q))
+#onesvec = ones(size(q))
 
-Adiff1 = sum((A.-A0).^2)
-Adiff2 = sum((A.-Anew).^2)
-oldf = sum((non_zero_values).^2)
-newf = sum((out.minimizer[ulength+1:end]).^2)
-tracer_cons1 = sum((Q⁻*A*cvec - q).^2)
-tracer_cons2 = sum((Q⁻*Anew*(cvec+out.minimizer[begin:ulength]) - q).^2)
-mass_cons1 = sum((A*onesvec).^2)
-mass_cons2 = sum((Anew*onesvec).^2)
+#oldf = sum((non_zero_values).^2)
+#newf = sum((out.minimizer[ulength+1:end]).^2)
+#tracer_cons1 = sum((Q⁻*A0*cvec - q).^2)
+#tracer_cons2 = sum((Q⁻*Anew*(cvec+out.minimizer[begin:ulength]) - q).^2)
+#mass_cons1 = sum((A0*onesvec).^2)
+#mass_cons2 = sum((Anew*onesvec).^2)
 
-
-println("A difference before: $Adiff1")
-println("A difference after: $Adiff2")
-println("old tracer cons:$tracer_cons1")
-println("new tracer cons:$tracer_cons2")
-println("old mass cons:$mass_cons1")
-println("new mass cons:$mass_cons2")
+#println("old tracer cons:$tracer_cons1")
+#println("new tracer cons:$tracer_cons2")
+#println("old mass cons:$mass_cons1")
+#println("new mass cons:$mass_cons2")
 
 b = getsurfaceboundary(θtrue)
-Alu2 = lu(Anew)
-final_θ = steadyinversion(Alu2,b,γ)
+
+#A0
+
+
+#Anew
+#Alu2 = lu(A0)
+
+
+final_θ = steadyinversion(Anew,b,γ)
 
 # plot the difference
-level = 15 # your choice 1-33
-depth = γ.depth[level]
+#level = 15 # your choice 1-33
+#depth = γ.depth[level]
 
-cntrs = 0:0.5:15
-label = "True θ"
-planviewplot(θtrue, depth, cntrs, titlelabel=label)
-readline()
+#cntrs = 0:0.5:15
+#label = "True θ"
+#planviewplot(θtrue, depth, cntrs, titlelabel=label)
+#readline()
 
-cntrs = 0:0.5:15
-label = "Final θ"
-planviewplot(final_θ, depth, cntrs, titlelabel=label)
-readline()
-
+#cntrs = 0:0.5:15
+#label = "Final θ"
+#planviewplot(final_θ, depth, cntrs, titlelabel=label)
+#readline()
--- scripts/open_MITgcm.jl
+++ scripts/open_MITgcm.jl
@@ -11,6 +11,7 @@ using GeoPythonPlot
 using LinearAlgebra
 using SparseArrays
 using Statistics
+using JLD
 #, Distributions, LinearAlgebra,  Zygote, ForwardDiff, Optim
 
 TMIversion = "simple_mitgcm"
@@ -24,18 +25,65 @@ pkgdatadir(args...) = joinpath(pkgdatadir(), args...)
 
 TMIfile = pkgdatadir(""*TMIversion*".nc")
 println(TMIfile)
-γ = Grid(TMIfile, "THETA", "XC", "YC", "Z", "maskC")
-ds = Dataset(TMIfile)
-#θtrue = convert(Vector{Float32},ds["THETA"]) 
+γ = Grid(TMIfile, "hFacC", "XC", "YC", "Z")
 θtrue = readfield(TMIfile,"THETA",γ)
 
-#println(γ)
+i_flatten = lonindex(γ.I)
+j_flatten = latindex(γ.I)
+k_flatten = depthindex(γ.I)
+
+#println(γ.I)
+ind_1 = zeros(0)
+ind_2 = zeros(0)
+
+# initialize A
+for ii in 1:length(i_flatten)
+      loc = zeros(0)
+      loc1 = findall((lonindex(γ.I) .== i_flatten[ii]) .& (latindex(γ.I) .== j_flatten[ii]) .& (depthindex(γ.I) .== k_flatten[ii]))
+      append!(loc,loc1)
+      if i_flatten[ii]>1
+          loc1 = findall((lonindex(γ.I) .== i_flatten[ii]-1) .& (latindex(γ.I) .== j_flatten[ii]) .& (depthindex(γ.I) .== k_flatten[ii]))
+          append!(loc,loc1)
+      end
+      if i_flatten[ii]<length(γ.lon)
+          loc1 = findall((lonindex(γ.I) .== i_flatten[ii]+1) .& (latindex(γ.I) .== j_flatten[ii]) .& (depthindex(γ.I) .== k_flatten[ii]))
+          append!(loc,loc1)
+      end
+      if i_flatten[ii]==1
+          loc1 = findall((lonindex(γ.I) .== length(γ.lon)) .& (latindex(γ.I) .== j_flatten[ii]) .& (depthindex(γ.I) .== k_flatten[ii]))
+          append!(loc,loc1)
+      end
+      if j_flatten[ii]>1
+          loc1 = findall((lonindex(γ.I) .== i_flatten[ii]) .& (latindex(γ.I) .== j_flatten[ii]-1) .& (depthindex(γ.I) .== k_flatten[ii]))
+          append!(loc,loc1)
+      end
+      if j_flatten[ii]<length(γ.lat)
+          loc1 = findall((lonindex(γ.I) .== i_flatten[ii]) .& (latindex(γ.I) .== j_flatten[ii]+1) .& (depthindex(γ.I) .== k_flatten[ii]))
+          append!(loc,loc1)
+      end
+      if k_flatten[ii]>1
+          loc1 = findall((lonindex(γ.I) .== i_flatten[ii]) .& (latindex(γ.I) .== j_flatten[ii]) .& (depthindex(γ.I) .== k_flatten[ii]-1))
+          append!(loc,loc1)
+      end
+      if k_flatten[ii]<length(γ.depth)
+          loc1 = findall((lonindex(γ.I) .== i_flatten[ii]) .& (latindex(γ.I) .== j_flatten[ii]) .& (depthindex(γ.I) .== k_flatten[ii]+1))
+          append!(loc,loc1)
+      end      
+      append!(ind_1,ii*ones(length(loc)))
+      append!(ind_2,loc)
+end
+
+println(size(ind_1),size(ind_2))
+save("ind_1.jld","ind_1", ind_1)
+save("ind_2.jld","ind_2", ind_2)
 
 # plot the difference
 level = 15 # your choice 1-33
 depth = γ.depth[level]
+lon = γ.lon
+lat = γ.lat
 
 cntrs = 0:0.5:15
 label = "True θ"
-planviewplot(θtrue, depth, cntrs, titlelabel=label)
+planviewplot(θtrue, depth, cntrs, titlelabel=label,cenlon=40)
 readline()
