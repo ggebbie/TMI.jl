e44fc97117b05759a3fb356c9d9c4256836ed7cf
anthony-meza <ameza98@outlook.com>
2024-02-16 11:18:11 -0600

    updating scripts from 2 years ago lol

--- scripts/deltaC13_regen.jl
+++ scripts/deltaC13_regen.jl
@@ -1,11 +1,10 @@
 #=
 % Example: Compute the spatial distribution of δ¹³C %
 =#
-
 using Revise
-using TMI, PyPlot, PyCall, Statistics
+using TMI, PyPlot, PyCall, Statistics, DrWatson
 
-pygui(false) 
+mkpath(plotsdir())
 
 TMIversion = "modern_90x45x33_G14_v2"
 A, Alu, γ, TMIfile, L, B = config_from_nc(TMIversion)
@@ -23,7 +22,7 @@ d_NATL = getRegion(TMIversion,"d_NATL")
 #plot the distribution at the surface
 fig = figure()
 ax = fig.add_subplot(projection = TMI.cartopy.crs.PlateCarree())
-ax.contourf(γ.lon,γ.lat,δ¹³C[:, :,lvl]' .* d_NATL' ,
+cax = ax.contourf(γ.lon,γ.lat,δ¹³C[:, :,lvl]' .* d_NATL' ,
             transform=TMI.cartopy.crs.PlateCarree()
             ) 
 ax.coastlines() #show coastlines
@@ -31,16 +30,16 @@ gl = ax.gridlines(draw_labels=true, dms=true, x_inline=false, y_inline=false)
 gl.top_labels = false
 gl.right_labels = false
 ax.set_title("δ¹³C d_NATL")
+fig.colorbar(cax, orientation = "horizontal")
+fig
 fig.savefig(plotsdir("δ¹³C_d_NATL.png"))
 
 
-x = δ¹³C[:, :,lvl]' .* d_NATL'
-δ¹³C_NA = mean(filter(!isnan,x)) 
-println("δ¹³C_NA: ", δ¹³C_NA)
+d_TROPPAC = getRegion(TMIversion,"d_TROPPAC")
 
 fig = figure()
 ax = fig.add_subplot(projection = TMI.cartopy.crs.PlateCarree())
-ax.contourf(γ.lon,γ.lat,δ¹³C[:, :,lvl]' .* d_SUBANTPAC' ,
+ax.contourf(γ.lon,γ.lat,δ¹³C[:, :,lvl]' .* d_TROPPAC' ,
             transform=TMI.cartopy.crs.PlateCarree()
             ) 
 ax.coastlines() #show coastlines
@@ -48,35 +47,36 @@ gl = ax.gridlines(draw_labels=true, dms=true, x_inline=false, y_inline=false)
 gl.top_labels = false
 gl.right_labels = false
 ax.set_title("δ¹³C d_TROPPAC")
-x = δ¹³C[:, :,lvl]' .* d_SUBANTPAC'
-δ¹³C_P = mean(filter(!isnan,x))
-fig.savefig(plotsdir("δ¹³C_d_NATL.png"))
+fig.colorbar(cax, orientation = "horizontal")
+fig
+fig.savefig(plotsdir("δ¹³C_d_TROPPAC.png"))
 
-δ¹³C_AS = δ¹³C_P - δ¹³C_NA
 
-println("δ¹³C_AS ", δ¹³C_AS)
+# δ¹³C_AS = δ¹³C_P - δ¹³C_NA
 
-x = PO₄ᴿ[:, :,lvl]' .* d_NATL'
-PO₄ᴿ_NA = mean(filter(!isnan,x))
-R = -3 / 2.5
+# println("δ¹³C_AS ", δ¹³C_AS)
 
-pred_δ¹³C = ones(length(axes(δ¹³C,3)))
-true_δ¹³C = similar(pred_δ¹³C)
-for deep_lvl in axes(δ¹³C,3)
-    x = PO₄ᴿ[:, :,deep_lvl]' .* d_SUBANTPAC'
-    PO₄ᴿ_DP = mean(filter(!isnan,x))
-    RΔPO₄ᴿ = R * (PO₄ᴿ_DP - PO₄ᴿ_NA)
-    pred_δ¹³C[deep_lvl] = δ¹³C_NA + RΔPO₄ᴿ + δ¹³C_AS
-    x = δ¹³C[:, :,deep_lvl]' .* d_SUBANTPAC'
-    true_δ¹³C[deep_lvl] = mean(filter(!isnan,x))
-end
+# x = PO₄ᴿ[:, :,lvl]' .* d_NATL'
+# PO₄ᴿ_NA = mean(filter(!isnan,x))
+# R = -3 / 2.5
 
-figs, axs = subplots(1,3, figsize = (10, 5))
-axs[1].plot(pred_δ¹³C, -depth)
-axs[1].set_title("pred_δ¹³C")
-axs[2].plot(true_δ¹³C, -depth)
-axs[2].set_title("true_δ¹³C")
-axs[3].plot(pred_δ¹³C .- true_δ¹³C, -depth)
-axs[3].set_title("pred_δ¹³C .- true_δ¹³C")
+# pred_δ¹³C = ones(length(axes(δ¹³C,3)))
+# true_δ¹³C = similar(pred_δ¹³C)
+# for level in size(δ¹³C,3)
+#     x = PO₄ᴿ[:, :,level]' .* d_SUBANTPAC'
+#     PO₄ᴿ_DP = mean(filter(!isnan,x))
+#     RΔPO₄ᴿ = R * (PO₄ᴿ_DP - PO₄ᴿ_NA)
+#     pred_δ¹³C[level] = δ¹³C_NA + RΔPO₄ᴿ + δ¹³C_AS
+#     x = δ¹³C[:, :,level]' .* d_SUBANTPAC'
+#     true_δ¹³C[level] = mean(filter(!isnan,x))
+# end
 
-println("Compared to: ", mean(filter(!isnan,x)))
+# figs, axs = subplots(1,3, figsize = (10, 5))
+# axs[1].plot(pred_δ¹³C, -depth)
+# axs[1].set_title("pred_δ¹³C")
+# axs[2].plot(true_δ¹³C, -depth)
+# axs[2].set_title("true_δ¹³C")
+# axs[3].plot(pred_δ¹³C .- true_δ¹³C, -depth)
+# axs[3].set_title("pred_δ¹³C .- true_δ¹³C")
+
+# println("Compared to: ", mean(filter(!isnan,x)))
