3e46933ae71377632e240d704b3b89e035539191
G Jake Gebbie <ggebbie@whoi.edu>
2024-02-18 10:49:59 -0500

    wip: invert foreign mitgcm tracers

---
+++ scripts/invert_mitgcm.jl
@@ -0,0 +1,15 @@
+import Pkg; Pkg.activate(".")
+using Revise
+using TMI
+fname = TMI.pkgdatadir("simple_MITgcm.nc")
+    println(fname)
+    γ =  Grid(fname, "maskC", "XC", "YC", "Z")
+
+y = (
+    θ = readfield(fname,"THETA",γ),
+    S = readfield(fname,"SALT",γ))
+
+@time m̃ = massfractions(y)
+
+
+end
