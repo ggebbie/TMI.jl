3e0deb11faff0cf7a73e8aa2c23efef4e5825531
Anthony Meza <ameza98@outlook.com>
2025-05-05 17:06:04 -0400

    update figures

--- scripts/random_profiles_comparison.jl
+++ scripts/random_profiles_comparison.jl
@@ -14,8 +14,9 @@ global_surface_average(x::AbstractArray, γ::Grid) = sum(replace(x[:, :, 1], NaN
 
 function extract_GH19_year(year::Int)
     # fname = "../GH19.jl/data/Theta_OPT-0015.nc"
-    fname = "../GH19.jl/data/Theta_EQ-1750.nc"
+    fname = "../GH19.jl/data/Theta_OPT-0015.nc"
     dataset = NCDataset(fname, "r")
+    println(dataset["year"][:])
     PI_index = findall(dataset["year"][:] .== year)[1]
     PI_theta = permutedims(dataset["theta"][PI_index, :, :, :], [3, 2, 1])
     return PI_theta
@@ -64,7 +65,7 @@ function generate_temperature_difference_plot(boot_profiles, LGM_theta, PI_theta
 
     # Define consistent plot style for all plots
     plot_style = Dict(
-        :xlabel => r"\$\\overline{\\Delta{SST}}(^\\circ C)\$", 
+        :xlabel => "\$\\overline{\\Delta{SST}}(^\\circ C)\$", 
         :ylabel => "\$\\overline{\\Delta{MOT}}(^\\circ C)\$",
         :title => "\$\\Delta\$Ocean Temp. (LGM vs. PI)", 
         :markerstrokewidth => 0.1, :markersize => 3,
@@ -82,6 +83,8 @@ function generate_temperature_difference_plot(boot_profiles, LGM_theta, PI_theta
     # Create the plot
     p = scatter(delta_sst, delta_mot, alpha = 0.6,
         label="Bootstrap avgs, $Nboot dots total, each dot is avg of $N_sample samples\n($sampling_description)"; plot_style...)
+    plot!(collect(plot_style[:xlims]), collect(plot_style[:xlims]), label="1:1",
+          linestyle=:dash, linewidth=2, color=:black)
 
     # Add mean and global average points
     highlight = Dict(:markerstrokewidth => 2, :markersize => 10, :order => 2)
@@ -93,16 +96,16 @@ function generate_temperature_difference_plot(boot_profiles, LGM_theta, PI_theta
     global_mot_diff = global_ocean_average(PI_theta_1850, γ_PI) - global_ocean_average(LGM_theta, γ_lgm)
     
     scatter!([global_sst_diff], [global_mot_diff], 
-        label="TMI Volume Weighted Differences\n(EQ-1750 minus G14)", color=:green; highlight...)
-    
+        label="TMI Volume Weighted Differences\n(OPT-0015 minus G14)", color=:green; highlight...)
+
     savefig(output_filename)
     return p
 end
 
 # Run analyses
 N_sample = 30
-Nboot = 2000
-PI_year =1850
+Nboot = 100
+PI_year =20 #15 CE Equillibriation
 
 #Running bootstrap with uniform sampling
 boot_profiles, LGM_theta, PI_theta_1850, γ_PI, γ_lgm = bootstrap_PI_lgm_differences(
