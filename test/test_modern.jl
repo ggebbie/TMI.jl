cd9af9238e31f1669f1e03847bcb1b7f095fbb6a
G Jake Gebbie <ggebbie@whoi.edu>
2025-06-05 22:20:06 -0400

    test "radioactive oxygen"

--- test/test_modern.jl
+++ test/test_modern.jl
@@ -66,7 +66,13 @@
         O₂ = steadyinversion(Alu,bO₂,γ,q=qPO₄,r=-170.0)
 
         ## radio-decay
-        Aradio = watermassmatrix(file, 5730) # like radiocarbon
+        Aradio = watermassmatrix(TMIfile, γ, 5730) # like radiocarbon
+        Oradio = steadyinversion(Aradio,bO₂,γ)
+
+        Aradio2 = watermassmatrix(TMIfile, γ, 269) # like radiocarbon
+        Oradio2 = steadyinversion(Aradio2,bO₂,γ)
+
+        @test minimum(Oradio) > minimum(Oradio2)
     end
 
     ############################
