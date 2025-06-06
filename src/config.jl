ae0f19293377012608469f1541fcb18e7479af92
G Jake Gebbie <ggebbie@whoi.edu>
2025-06-05 22:20:31 -0400

    construct water-mass matrix with radio-decay

--- src/config.jl
+++ src/config.jl
@@ -144,18 +144,23 @@ function watermassmatrix(file)
     return A
 end
 
-function watermassmatrix(file, halflife)
+function watermassmatrix(file, γ, halflife)
 
-    #!haskey(NCDataset(file),"F₀") && error("watermassmatrix: no rates included with this version")
+    !haskey(NCDataset(file),"F₀") && error("watermassmatrix: no rates included with this version")
     Aradio = watermassmatrix(file) # placeholder
     F₀ = ncread(file,"F₀")
 
     # decay constant
     λ = log(2)/halflife
-    diag_decay = (- λ ./ F₀)[γ.wet]
-    remove_inf = x -> (isinf(x) ? 0.0 : x) # use pair substitution instead
-    replace!(remove_inf, diag_decay)
-    return Aradio -= Diagonal(diag_decay)
+    diag_decay = (- λ ./ F₀)
+    ml = mixedlayermask(Aradio, γ)
+    bl = boundarymask(γ)
+    diag_decay[ml] .= 0.0
+    diag_decay[bl] .= 0.0
+    
+    # remove_inf = x -> (ml(x) ? 0.0 : x) # use pair substitution instead
+    # replace!(remove_inf, diag_decay)
+    return Aradio += Diagonal(diag_decay[γ.wet])
 end
 
 """
