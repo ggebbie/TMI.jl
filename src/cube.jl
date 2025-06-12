278c6f9f8d6db948d0bff2a8559c973e5721b49e
Anthony Meza <ameza98@outlook.com>
2025-06-12 11:33:28 -0400

    pushing a working inversion of lateral and verticalboundary conditions

--- src/cube.jl
+++ src/cube.jl
@@ -1,47 +1,53 @@
 
 function get_above_cube_boundary_condition(xlim, ylim, zlim, γ::Grid)
+    #this is binary boundary condition
     cube_up    = TMI.above_cube(xlim, ylim, zlim, γ)
     A          = Float64.(cube_up)
     cube_flat  = sum(A, dims=(1,2))[:]
-    kidx       = findlast(cube_flat .> 0)
-    fld        = Field(A .* γ.wet, γ, :nothing, "upper boundary condition", "nothing")
-    return TMI.getboundarycondition(fld, 3, kidx, γ)
+    dimval       = findlast(cube_flat .> 0)
+    A[γ.wet .== 0] .= NaN
+    fld        = Field(A, γ, :nothing, "upper boundary condition", "nothing")
+    return TMI.getboundarycondition(fld, 3, dimval, γ)
 end
 
 function get_below_cube_boundary_condition(xlim, ylim, zlim, γ::Grid)
     cube_down  = TMI.below_cube(xlim, ylim, zlim, γ)
     A          = Float64.(cube_down)
     cube_flat  = sum(A, dims=(1,2))[:]
-    kidx       = findfirst(cube_flat .> 0)
-    fld        = Field(A  .* γ.wet, γ, :nothing, "lower boundary condition", "nothing")
-    return TMI.getboundarycondition(fld, 3, kidx, γ)
+    dimval       = findfirst(cube_flat .> 0)
+    A[γ.wet .== 0] .= NaN
+    fld        = Field(A, γ, :nothing, "lower boundary condition", "nothing")
+    return TMI.getboundarycondition(fld, 3, dimval, γ)
 end
 
 function get_east_of_cube_boundary_condition(xlim, ylim, zlim, γ::Grid)
     cube_east  = TMI.east_of_cube(xlim, ylim, zlim, γ)
     A          = Float64.(cube_east)
     cube_flat  = sum(A, dims=(2,3))[:]
-    lonidx     = findlast(cube_flat .> 0)
-    fld        = Field(A .* γ.wet, γ, :nothing, "eastern boundary condition", "nothing")
-    return TMI.getboundarycondition(fld, 1, lonidx, γ)
+    dimval     = findfirst(cube_flat .> 0)
+    A[γ.wet .== 0] .= NaN
+    fld        = Field(A, γ, :nothing, "eastern boundary condition", "nothing")
+    return TMI.getboundarycondition(fld, 1, dimval, γ)
 end
 
 function get_west_of_cube_boundary_condition(xlim, ylim, zlim, γ::Grid)
     cube_west  = TMI.west_of_cube(xlim, ylim, zlim, γ)
     A          = Float64.(cube_west)
     cube_flat  = sum(A, dims=(2,3))[:]
-    lonidx     = findfirst(cube_flat .> 0)
-    fld        = Field(A  .* γ.wet, γ, :nothing, "western boundary condition", "nothing")
-    return TMI.getboundarycondition(fld, 1, lonidx, γ)
+    dimval     = findlast(cube_flat .> 0)
+    A[γ.wet .== 0] .= NaN
+    fld        = Field(A, γ, :nothing, "western boundary condition", "nothing")
+    return TMI.getboundarycondition(fld, 1, dimval, γ)
 end
 
 function get_south_of_cube_boundary_condition(xlim, ylim, zlim, γ::Grid)
     cube_south = TMI.south_of_cube(xlim, ylim, zlim, γ)
     A          = Float64.(cube_south)
     cube_flat  = sum(A, dims=(1,3))[:]
-    latidx     = findlast(cube_flat .> 0)
-    fld        = Field(A .* γ.wet, γ, :nothing, "southern boundary condition", "nothing")
-    return TMI.getboundarycondition(fld, 2, latidx, γ)
+    dimval     = findlast(cube_flat .> 0)
+    A[γ.wet .== 0] .= NaN
+    fld        = Field(A, γ, :nothing, "southern boundary condition", "nothing")
+    return TMI.getboundarycondition(fld, 2, dimval, γ)
 end
 
 
@@ -49,7 +55,8 @@ function get_north_of_cube_boundary_condition(xlim, ylim, zlim, γ::Grid)
     cube_north = TMI.north_of_cube(xlim, ylim, zlim, γ)
     A          = Float64.(cube_north)  
     cube_flat  = sum(A, dims=(1,3))[:]
-    latidx     = findfirst(cube_flat .> 0)
-    fld        = Field(A .* γ.wet, γ, :nothing, "northern boundary condition", "nothing")
-    return TMI.getboundarycondition(fld, 2, latidx, γ)
+    dimval     = findfirst(cube_flat .> 0)
+    A[γ.wet .== 0] .= NaN
+    fld        = Field(A, γ, :nothing, "northern boundary condition", "nothing")
+    return TMI.getboundarycondition(fld, 2, dimval, γ)
 end
