31b3a73f0952e5eed7e3eeb01335994158c1c9b5
G Jake Gebbie <ggebbie@whoi.edu>
2025-06-18 19:11:07 -0400

    opt-15 north pacific heat budget

--- scripts/npac_watermass_meanage.jl
+++ scripts/npac_watermass_meanage.jl
@@ -9,6 +9,8 @@ using LinearAlgebra
 using TMI
 using GeoPythonPlot # will load optional extension
 using SparseArrays
+using GH19
+using NCDatasets
 
 versionno = 2
 if versionno == 1
@@ -53,6 +55,7 @@ b_ones = (surface = b_surface,
 
 # update water-mass matrix to be consistent with boundary points and grid 
 Abc = watermassmatrix(A, γb)
+Abc_lu = Abc_lu
 
 ## get average over cube
 ## big control volume to stabilize results
@@ -79,7 +82,7 @@ b_s  = (surface = 0.0*b_surface,
           south = true*b_south,
           upper = false*b_up,
           lower = false*b_lo)
-m_south = steadyinversion(lu(Abc),b_s,γb)
+m_south = steadyinversion(Abc_lu,b_s,γb)
 maximum(m_south)
 minimum(m_south)
 
@@ -92,7 +95,7 @@ b_l  = (surface = 0.0*b_surface,
           south = 0.0*b_south,
           upper = 0.0*b_up,
           lower = 1.0*b_lo)
-m_lower = steadyinversion(lu(Abc),b_l,γb)
+m_lower = steadyinversion(Abc_lu,b_l,γb)
 maximum(m_lower)
 minimum(m_lower)
 mbar_lower = cube_mean(m_lower) # 0.51/0.51
@@ -102,7 +105,7 @@ b_u  = (surface = 0.0*b_surface,
           south = 0.0*b_south,
           upper = 1.0*b_up,
           lower = 0.0*b_lo)
-m_upper = steadyinversion(lu(Abc),b_u,γb)
+m_upper = steadyinversion(Abc_lu,b_u,γb)
 mbar_upper = cube_mean(m_upper) # 0.32/0.32
 
 ##surface boundary (non-local)
@@ -110,13 +113,10 @@ b_srf  = (surface = 1.0*b_surface,
           south = 0.0*b_south,
           upper = 0.0*b_up,
           lower = 0.0*b_lo)
-m_srf = steadyinversion(lu(Abc),b_srf,γb)
+m_srf = steadyinversion(Abc_lu,b_srf,γb)
 mbar_srf = cube_mean(m_srf) # 0.002/0.0003
 
-maximum(m_lower)
-minimum(m_lower)
-
-msum = cube_mean(m_srf) + cube_mean(m_south) + cube_mean(m_lower) + cube_mean(m_upper)
+msum = mbar_srf + mbar_south + mbar_lower + mbar_upper
 
 a_boundary = (surface =zeros(3,1,γb,:bc_surface,"Surface","nondim"), 
               south = zeros(2,30,γb,:bc_south,"South Boundary","nondim"),
@@ -153,10 +153,10 @@ sectionplot(a_npac,lon_section,lims,titlelabel = tlabel,fname=TMI.pkgplotsdir("a
 θ_cube = cube_mean(θ)
 
 # get effective endmember
-θ_south = TMI.effective_endmember(lu(Abc),θ,b_s,γb, control_volume= cube)
-θ_upper = TMI.effective_endmember(lu(Abc),θ,b_u,γb, control_volume= cube)  
-θ_lower = TMI.effective_endmember(lu(Abc),θ,b_l,γb, control_volume= cube)
-θ_srf = TMI.effective_endmember(lu(Abc),θ,b_srf,γb, control_volume= cube)
+θ_south = TMI.effective_endmember(Abc_lu,θ,b_s,γb, control_volume= cube)
+θ_upper = TMI.effective_endmember(Abc_lu,θ,b_u,γb, control_volume= cube)  
+θ_lower = TMI.effective_endmember(Abc_lu,θ,b_l,γb, control_volume= cube)
+θ_srf = TMI.effective_endmember(Abc_lu,θ,b_srf,γb, control_volume= cube)
 
 θprime_south = θ_south - θ_cube
 θprime_upper = θ_upper - θ_cube
@@ -167,3 +167,35 @@ Fθ_south = 10000*mbar_south*θprime_south / τ_npac # cK/century
 Fθ_upper = 10000*mbar_upper*θprime_upper / τ_npac # cK/century 
 Fθ_lower = 10000*mbar_lower*θprime_lower / τ_npac # cK/century 
 Fθ_srf = 10000*mbar_srf*θprime_srf / τ_npac # cK/century 
+
+# try with GH19 anomaly fields
+anomaly = true
+gh19filename = download_exp("OPT-0015",anomaly)
+ds = NCDataset(gh19filename)
+θgh19 = GH19.readfield_snapshot(gh19filename,"theta", 11, γ)
+
+# test what the actual temperature change is
+Δθgh19 = 100*cube_mean( GH19.readfield_snapshot(gh19filename,"theta", 1, γ) -
+                    GH19.readfield_snapshot(gh19filename,"theta", 21, γ))
+
+θgh19_cube = cube_mean(θgh19) # 0.165
+θgh19_south = TMI.effective_endmember(Abc_lu,θgh19,b_s,γb, control_volume= cube) # 0.168
+θgh19_upper = TMI.effective_endmember(Abc_lu,θgh19,b_u,γb, control_volume= cube)  # 0.157
+θgh19_lower = TMI.effective_endmember(Abc_lu,θgh19,b_l,γb, control_volume= cube)  # 0.157
+θgh19_srf = TMI.effective_endmember(Abc_lu,θgh19,b_srf,γb, control_volume= cube) # 0.30
+
+θgh19prime_south = θgh19_south - θgh19_cube # 0.003 
+θgh19prime_upper = θgh19_upper - θgh19_cube # -0.007
+θgh19prime_lower = θgh19_lower - θgh19_cube # -0.008
+θgh19prime_srf = θgh19_srf - θgh19_cube # 0.24
+
+Fθgh19_south = 10000*mbar_south*θgh19prime_south / τ_npac # 0.15 cK/century 
+Fθgh19_upper = 10000*mbar_upper*θgh19prime_upper / τ_npac # -0.64 cK/century 
+Fθgh19_lower = 10000*mbar_lower*θgh19prime_lower / τ_npac # -1.18 cK/century 
+Fθgh19_srf = 10000*mbar_srf*θgh19prime_srf / τ_npac # 0.013 cK/century
+
+Fθgh19_south + Fθgh19_upper + Fθgh19_lower + Fθgh19_srf
+# compare to Δθgh19
+percent_error = 100* (Δθgh19 - (Fθgh19_south + Fθgh19_upper + Fθgh19_lower + Fθgh19_srf)) /
+                (Δθgh19 + (Fθgh19_south + Fθgh19_upper + Fθgh19_lower + Fθgh19_srf)) 
+
