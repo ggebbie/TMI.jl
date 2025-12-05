5359f01a9d58e25dce8a83535e06f984fe23ed1f
Anthony Meza <64243783+anthony-meza@users.noreply.github.com>
2025-12-04 19:02:38 -0500

    adding empty control and observation test files

---
+++ test/test_controls.jl
@@ -0,0 +1,4 @@
+# make a control object (from 3d field) 
+
+# try the unvec!, unvec and vec things. 
+
---
+++ test/test_observations.jl
@@ -0,0 +1,5 @@
+# create observation structs  
+
+# test a single observe and gobserve for 1d and 3d fields.
+
+# confirm that they are consistent with the non-observation functions 
